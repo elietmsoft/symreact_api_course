2a38328d987e123e05bcf70d88802a08f304601c
elietmsoft <elietshibangu347@gmail.com>
2020-10-30 11:42:28 +0100

    arrangement

--- src/Entity/Customer.php
+++ src/Entity/Customer.php
@@ -5,10 +5,12 @@ namespace App\Entity;
 use App\Repository\CustomerRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
+use ApiPlatform\Core\Annotation\ApiResource;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
  * @ORM\Entity(repositoryClass=CustomerRepository::class)
+ * @ApiResource
  */
 class Customer
 {
